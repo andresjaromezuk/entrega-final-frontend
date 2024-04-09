80ed03b57e5efb7f3ae7b05996de10c5f000b9d2
andresjaromezuk <andres.jaromezuk@gmail.com>
2024-04-08 23:49:42 -0300

    adding dotenv

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express, { json, urlencoded } from 'express'
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT 
 app.use('/', express.static('./www'))
 
 app.listen(PORT, () => console.log(`Servidor de frontend escuchando en puerto ${PORT}`))
