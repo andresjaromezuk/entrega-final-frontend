c5b06adde34b567f1e21e1f582b9ad9d25ed2ae4
andresjaromezuk <andres.jaromezuk@gmail.com>
2024-04-09 22:22:46 -0300

    fixing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express, { json, urlencoded } from 'express'
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
 
-import path from 'path'
-import dotenv from 'dotenv';
 dotenv.config()
 
-import {fileURLToPath} from 'url'
-import {dirname} from 'path'
+//import {fileURLToPath} from 'url'
+// import {dirname} from 'path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
 
-export default __dirname
+// export default __dirname
 
 const app = express()
 const PORT = process.env.PORT || 3000
